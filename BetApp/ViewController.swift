6d617620b9a9c8dac934eb3b5a3218b540becf03
seregious <seregious@gmail.com>
2022-05-23 11:08:52 +0300

    Initial Commit

---
+++ BetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BetApp
+//
+//  Created by Сергей Иванчихин on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
